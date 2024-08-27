3cdbbf223c773153132472702b857ba564a84ae9
diak345 <diak34560@gmail.com>
2024-08-27 13:51:28 +0900

    first commit

---
+++ main.js
@@ -0,0 +1,18 @@
+var WebSocket = require('ws');
+var ws = WebSocket.Server;
+var wss = new ws({port: 3001});
+
+wss.on("connection", function (ws) {
+    ws.on("message", function (message, isBinary) {
+        wss.clients.forEach(function each(client) {
+            if (ws === client) {
+                console.log("- skip sender -");
+                var enc = new TextDecoder("utf-8");
+                var arr = new Uint8Array(message);
+                console.log(enc.decode(arr));
+            } else {
+                client.send(message, { binary: isBinary });
+            }
+        });
+    });
+});
